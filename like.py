00f4965b94cde8ef8e45fcdf3553bd31e31b3043
ErikBoesen <me@erikboesen.com>
2019-02-17 22:27:13 -0500

    Add starting script to do logging

---
+++ like.py
@@ -0,0 +1,36 @@
+import requests
+import argparse
+
+GROUP_ID = 46649296
+MY_ID = 41430499
+
+
+parser = argparse.ArgumentParser(description='Analyze a GroupMe chat')
+parser.add_argument('token', help='Your GroupMe developer token')
+args = parser.parse_args()
+
+def get(path, params=None):
+    return requests.get('https://api.groupme.com/v3/' + path + '?token=' + args.token, params=params).json()['response']
+
+group = get('groups/%d' % GROUP_ID)
+
+message_id = 0
+message_number = 0
+while message_number < group['messages']['count']:
+    params = {
+        # Get maximum number of messages at a time
+        'limit': 100,
+    }
+    if message_id:
+        params['before_id'] = message_id
+    messages = get('groups/%s/messages' % group['id'], params)['messages']
+    for message in messages:
+        message_number += 1
+
+        text = message['text'] or ''
+        print(text)
+
+
+    message_id = messages[-1]['id']  # Get last message's ID for next request
+    remaining = 100 * message_number / group['messages']['count']
+    print('\r%.2f%% done' % remaining, end='')
