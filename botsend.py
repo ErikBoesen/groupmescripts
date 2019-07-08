9c333b8a6ff5d30785516e16c94f393d488b0522
ErikBoesen <me@erikboesen.com>
2019-07-08 12:50:01 -0400

    Add botsend.py

---
+++ botsend.py
@@ -0,0 +1,9 @@
+import requests
+
+GROUP_ID = 48071223
+
+bot_id = input("Bot ID: ")
+while True:
+    text = input("> ")
+    r = requests.post("https://api.groupme.com/v3/bots/post", data={"bot_id": bot_id, "text": text})
+    print(r.json())
